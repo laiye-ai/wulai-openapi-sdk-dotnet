68f25d51b40c435bf0dad2f76167be23cabafe36
YangJiangtao <yangjiangtao@laiye.com>
2019-08-27 16:23:01 +0800

    add test case HTTP

---
+++ WulAiSDK-Tests/HTTPTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+using WulAiSDK;
+using System.Threading.Tasks;
+using WulAiSDK.Util;
+using Newtonsoft.Json;
+using WulAiSDK.Request;
+
+namespace WulAiSDK_Tests
+{
+    public class HTTPTest
+    {
+        public async Task PostAPITest()
+        {
+            string pubkey = "HOn8N3JudtsASYsEQObvsXzHTWtg5i9X000a3e870eb2eddeb8";
+            string secret = "wKDQ5CYpScb56PfoWKBL";
+            string expected = await HTTP.PostAPI(pubkey, secret, WulAiAPI.CreateUser, JsonConvert.SerializeObject(new CreateUser()
+            {
+                user_id = "test_user_id",
+                nickname = "test_nickname"
+            }));
+
+
+            string actual = "{}";
+
+            Assert.Equal(expected, actual);
+
+
+
+
+
+        }
+
+
+
+    }
+}
